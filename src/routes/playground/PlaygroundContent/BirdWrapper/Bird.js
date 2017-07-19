707c0683fb97a8916210cd8156f5df95e0ba70a4
Korisnik <vrafaeli@msn.com>
2017-07-19 18:25:38 +0200

    Refactor of bird and its container

--- src/routes/playground/PlaygroundContent/BirdWrapper/Bird.js
+++ src/routes/playground/PlaygroundContent/BirdWrapper/Bird.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Animated, Text, View, TextInput, Button, Image, TouchableNativeFeedback  } from 'react-native';
+import { Animated } from 'react-native';
 
 class Bird extends React.Component {
 
@@ -9,12 +9,7 @@ class Bird extends React.Component {
         fadeAnim: new Animated.Value(0),
         leftAnim: new Animated.Value(10),
         topAnim: new Animated.Value(0),
-        toValue: 0,
         springMotion: null,
-        topContainerAnim: new Animated.Value(0),
-        fallingMotion: null,
-        topOffset: 0,
-        touchEnabled: true
     }
 
     springMotion = () => {
@@ -25,91 +20,30 @@ class Bird extends React.Component {
         this.state.springMotion.start();
     };
 
-    fallingMotion = () => {
-        this.state.fallingMotion = Animated.timing(
-            this.state.topContainerAnim, { toValue: 10000, duration: 60000 }
-        );
-
-        this.state.topContainerAnim.addListener((value) => {
-            console.log(value);
-            this.state.topOffset = value.value;
-            if (value.value > 320) {
-                this.state.fallingMotion.stop();
-                this.state.topContainerAnim.setValue(320);
-            }
-        });
-    }
-
-
     componentDidMount() {
-        Animated.timing(                  // Animate over time
-            this.state.fadeAnim,            // The animated value to drive
-            {
-                toValue: 1,                   // Animate to opacity: 1 (opaque)
-                duration: 5000,              // Make it take a while
-            }
-        ).start();                        // Starts the animation
+        Animated.timing( this.state.fadeAnim, { toValue: 1, duration: 5000, }).start();
 
-        Animated.timing(
-            this.state.leftAnim,
-            {
-                toValue: 70,
-                duration: 5000,
-            }
-        ).start();
-
-        this.fallingMotion();
-        
-        this.state.fallingMotion.start();
+        Animated.timing( this.state.leftAnim, { toValue: 70, duration: 5000 }).start();
 
         this.springMotion(10);
     }
 
-    _onPressButton = () => {
-
-        if (!this.state.touchEnabled) return;
-
-        this.state.touchEnabled = false;
-
-        this.state.fallingMotion.stop();
-
-        Animated.timing(
-            this.state.topContainerAnim,
-            {
-                toValue: this.state.topOffset - 50,
-                duration: 500,
-            }
-        ).start(() => {
-            this.state.fallingMotion.start();
-            this.state.touchEnabled = true;
-        });
-    }
 
     render () {
         return (
-            <Animated.View style={{
-                top: this.state.topContainerAnim,
-            }}>
-
-                <TouchableNativeFeedback  
-                    onPress={this._onPressButton}>
-            
-                    <Animated.Image 
-                        style={{
-                            position: 'relative',
-                            width: 45,
-                            height: 45,
-                            left: this.state.leftAnim,
-                            opacity: this.state.fadeAnim,
-                            top: this.state.topAnim,
-                        }}
                         
-                        source={require('./bird-icon2.png')}
-                    />
-                        
-                </TouchableNativeFeedback>
-
-            </Animated.View>
+            <Animated.Image 
+                style={{
+                    position: 'relative',
+                    width: 45,
+                    height: 45,
+                    left: this.state.leftAnim,
+                    opacity: this.state.fadeAnim,
+                    top: this.state.topAnim,
+                }}
+                
+                source={require('./bird-icon2.png')}
+            />
         );
     }
 }
--- src/routes/playground/PlaygroundContent/BirdWrapper/BirdWrapper.js
+++ src/routes/playground/PlaygroundContent/BirdWrapper/BirdWrapper.js
@@ -1,16 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
+import { Animated, TouchableNativeFeedback  } from 'react-native';
 
-import {Bird} from './Bird';
+import { Bird } from './Bird'; 
 
 class BirdWrapper extends React.Component {
+
+    state = {
+        toValue: 0,
+        topContainerAnim: new Animated.Value(0),
+        fallingMotion: null,
+        topOffset: 0,
+        touchEnabled: true
+    }
+
+    fallingMotion = () => {
+        this.state.fallingMotion = Animated.timing(
+            this.state.topContainerAnim, { toValue: 10000, duration: 60000 }
+        );
+
+        this.state.topContainerAnim.addListener((value) => {
+            console.log(value);
+            this.state.topOffset = value.value;
+            if (value.value > 320) {
+                this.state.fallingMotion.stop();
+                this.state.topContainerAnim.setValue(320);
+            }
+        });
+    }
+
+    componentDidMount() {
+
+        this.fallingMotion();
+        
+        this.state.fallingMotion.start();
+    }
+
+
+    _onPressButton = () => {
+
+        if (!this.state.touchEnabled) return;
+
+        this.state.touchEnabled = false;
+
+        this.state.fallingMotion.stop();
+
+        Animated.timing(
+            this.state.topContainerAnim,
+            {
+                toValue: this.state.topOffset - 50,
+                duration: 500,
+            }
+        ).start(() => {
+            this.state.fallingMotion.start();
+            this.state.touchEnabled = true;
+        });
+    }
+
     render () {
         return (
-            <View style={{width: '100%', height: '100%', top: '0%', position: 'absolute', zIndex: 100, backgroundColor: 'transparent'}}>
-                <Bird />
-            </View>
+            <Animated.View style={{
+                top: this.state.topContainerAnim,
+                zIndex: 100,
+                backgroundColor: 'transparent',
+                position: 'absolute',
+                width: '100%',
+                height: '100%'
+            }}>
+
+                <TouchableNativeFeedback  
+                    onPress={this._onPressButton}>
+
+                    <Bird />
+
+                </TouchableNativeFeedback>
+
+            </Animated.View>
         );
     }
 }
